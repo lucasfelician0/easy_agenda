6e86b309714bc5404a776a452af0b96a4be35c5e
Lucas Feliciano <lucasfelicianosoares@gmail.com>
2026-09-06 23:10:12 -0300

    trabalhando com validadores

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,26 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 , maximum: 100, allow_blank: true }
+  validates :started_at, presence: true
+  validates :finishead_at, presence: true
+  validate :validate_if_started_in_future, on: :create
+  validate :validate_if_finished_greater_tan_started_at
+
+
+  private
+
+  def validate_if_finished_greater_tan_started_at
+    return unless started_at
+    return unless finishead_at
+    return if finishead_at > started_at
+
+    errors.add(:finishead_at, :invalid)
+  end
+
+  def validate_if_started_in_future
+    return unless started_at
+    return if started_at >= Time.current
+
+    errors.add(:started_at, :invalid)
+  end
 end
